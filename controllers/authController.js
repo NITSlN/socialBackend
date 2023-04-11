2ef785bf95f44ac49ee2058f340161a98eaa197f
nitish@gaga <nitishgoku01@gmail.com>
2023-04-11 21:30:44 +0530

    modifying loginUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,15 @@ const { generateToken } = require("../utils/helper")
 const User = require("../models/UserSchema")
 const loginUser = async (req, res) => {
   try {
-
-    let email = 'abc@gmail.com'
+    const {email,password} = req.body
+    if(!email || !password) throw new Error("Email and Password is required")
+    const name = email.split('@')[0]
+    const user = await User.findOne({email})
+    if(!user){
+      await User.create({
+        email,password,name
+      })
+    }
     token = generateToken(email)// using dummy id as 1
     res
       .cookie('access_token', token, {
@@ -20,7 +27,6 @@ const loginUser = async (req, res) => {
 }
 
 const createUser = async (req, res)=>{
-  
   try {
     const user = await User.create(req.body)
     res.status(201).json(user)
